a33912cd7d561ebd7669b049f355058e3e9d652e
Randhall Rangel <randhall.rangel@neoris.com>
2020-08-14 19:39:03 -0300

    Include Navbar in React

--- StackModules/notesapp/src/App.js
+++ StackModules/notesapp/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import CreateNote from './Components/CreateNote';
+import CreateUser from './Components/CreateUser';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
+import NotesList from './Components/NotesList';
 
 function App() {
   return (
-    <div className="App">
-    <Navbar/>
-
-      Notes App 
-    <Footer/>
-    </div>
+    <Router>
+      <Navbar/>
+      <Route path='/' component = { NotesList }  />
+      <Route path='/edit/:id' component = { CreateNote } />
+      <Route path='/create' component = { CreateNote } />
+      <Route path='/user' component = { CreateUser } />
+      <Footer/>
+    </Router>
   );
 }
 
